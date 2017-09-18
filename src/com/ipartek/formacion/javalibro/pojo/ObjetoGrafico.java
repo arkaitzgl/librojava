ed73a67cf54433986e7f89c6d40f3cd60590c6c9
Administrador <Administrador@Formacion-TOS15.ipartekaula.com>
2017-09-18 14:32:10 +0200

    inicio clases abstractas

---
+++ src/com/ipartek/formacion/javalibro/pojo/ObjetoGrafico.java
@@ -0,0 +1,26 @@
+package com.ipartek.formacion.javalibro.pojo;
+
+/**
+ * Una clase abstarc puede contener codigo implementado
+ * pero tiene que tener algun metodo SIN implementar (abstrac)
+ * Es algo intermedio entre Class e Interface
+ * No puede instanciar objetos, pero de una interfaz si se puede
+ * Las clases hijas deben implementar los metodos abstractos o volver a declararlos abstrac
+ * 
+ * @author Administrador
+ *
+ */
+
+public abstract class ObjetoGrafico {
+	
+	int x;
+	int y;
+	
+	void mover (int xPos, int yPos) {
+		this.x=xPos;
+		this.y=yPos;
+	}
+
+	abstract void dibujar () ;
+	
+}
